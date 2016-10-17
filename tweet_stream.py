9c684e58d19ec25755653528beca6db03ebb1103
Jodie Gardiner <jodiegardiner@outlook.com>
2016-10-17 16:37:19 +0100

    more playing with charting tweet data

--- tweet_stream.py
+++ tweet_stream.py
@@ -1,33 +1,36 @@
 from tweepy import Stream
-from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
+from twit_api import get_auth
 
-CONSUMER_KEY = 'lPNchIfwCLd0AnJbLG6IfHyvY'
-CONSUMER_SECRET = 'ICm639pX5uWmvtYrYR9NysrrZAQQCdZbl9VFjVXXAXl9SkIp30'
-OAUTH_TOKEN = '20432318-seggLOx73igoOf37kNCYr40ViTI91fokO8rwG041m'
-OAUTH_TOKEN_SECRET = 'YKDzwih61fusSgwt15K79rxHbELNUZjNoapKJwlkqfULf'
+
 
 keyword_list = ['starwars', 'rogue one']  # track list
-limit = 100
+limit = 500
 
 
 class MyStreamListener(StreamListener):
+    def __init__(self):
+        super(MyStreamListener, self).__init__()
+        self.num_tweets = 0
+
     def on_data(self, data):
-        try:
-            with open('tweet_mining.json', 'a') as tweet_file:
-                tweet_file.write(data)
-                return True
-        except BaseException as e:
-            print("Failed on_data: %s" % str(e))
-        return True
+        if self.num_tweets < limit:
+            self.num_tweets += 1
+            try:
+                with open('tweet_mining2.json', 'a') as tweet_file:
+                    tweet_file.write(data)
+                    return True
+            except BaseException as e:
+                print ("Failed on_data: %s" % str(e))
+            return True
+        else:
+            return False
 
     def on_error(self, status):
         print(status)
         return True
 
-
-auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+auth = get_auth()
 
 twitter_stream = Stream(auth, MyStreamListener())
 twitter_stream.filter(track=keyword_list)
--- twit_api.py
+++ twit_api.py
@@ -1,12 +1,18 @@
 import tweepy
 from tweepy import OAuthHandler
 
-
 CONSUMER_KEY = 'lPNchIfwCLd0AnJbLG6IfHyvY'
 CONSUMER_SECRET = 'ICm639pX5uWmvtYrYR9NysrrZAQQCdZbl9VFjVXXAXl9SkIp30'
 OAUTH_TOKEN = '20432318-seggLOx73igoOf37kNCYr40ViTI91fokO8rwG041m'
 OAUTH_TOKEN_SECRET = 'YKDzwih61fusSgwt15K79rxHbELNUZjNoapKJwlkqfULf'
 
-auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-api = tweepy.API(auth)
+def get_auth():
+    auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+    return auth
+
+def get_api():
+    auth = get_auth()
+    return tweepy.API(auth)
+
+# api = tweepy.API(auth)
